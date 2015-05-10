f6cb51e0fa19c95738dddbfd6a6a5f1246664de8
Gerhard Pachl <gerhard.pachl@bluewin.ch>
2015-05-10 18:21:13 +0200

    Upd plots

--- plot3.r
+++ plot3.r
@@ -5,36 +5,33 @@
       # 2 - laundry room, washing-machine, tumble-drier, refrigerator, light (red colour)
       # 3 - electric water-heater, air-conditioner (blue colour)
       
-      # set my working directory
+      # set working directory
       setwd("C:/Users/Gerhard/ExData_Plotting1")
       
       ## import course project data from file "Individual household electric power consumption Data Set"  
-      hh_power_cons_data <- read.table("./household_power_consumption.txt",header = TRUE, sep = ";",na.strings="?",stringsAsFactors=FALSE, dec=".")
+      hh_power_cons_data <- read.table("./household_power_consumption.txt",header = TRUE, sep = ";",na.strings="?",stringsAsFactors=FALSE)
       
       # reduce the dataset entries: select the relevant data for the plot - dates 1/2/2007 and 2/2/2007
       hh_power_consumption <- subset(hh_power_cons_data, hh_power_cons_data$Date == "1/2/2007" | hh_power_cons_data$Date == "2/2/2007") 
       
       #  # create new variable "Date_time" for the plot - bind date and time and set appropriate format for the plot 
       hh_power_consumption$Date_time <- as.POSIXct(strptime(paste(hh_power_consumption$Date, hh_power_consumption$Time), "%d/%m/%Y %H:%M:%S"))      
-     
-      # convert Date-Format from variable "Date" also (from dd/mm/yyyy to default format yyyy-mm-dd)
-      hh_power_consumption$Date <- as.Date(hh_power_consumption$Date, format="%d/%m/%Y")
       
       # open graphics device png for plot3 (set width of 480 pixels and a height of 480 pixels)
       png("plot3.png", width=480, height=480)
       
-      # create a multiple plot for the three types of "energy sub-metering"  
-      with(hh_power_consumption, {
-            plot(Sub_metering_1~Date_time, type="l", ylab="Energy sub metering", xlab="")
-            lines(Sub_metering_2~Date_time,col='Red')
-            lines(Sub_metering_3~Date_time,col='Blue')
-      })
-      # add some labels and set properties for linetype
-      legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
-             legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+      # set some general parameters for graphics environment  
+      par(lty=1, lwd=1, lend=1, las=1)
+
+      # make plots for the three "energy sub-metering" consumption-types:
+      with (hh_power_consumption,plot(Sub_metering_1~Date_time, type = "l", ylab="Energy sub metering", xlab="",col="black"))
+      with (hh_power_consumption,lines(Sub_metering_2~Date_time,  type = "l" ,col="red"))
+      with (hh_power_consumption,lines(Sub_metering_3~Date_time,  type = "l", col="blue"))      
+
+      # add legend at topright
+      legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
       
       # close png graphics device
       dev.off()
-
 
 
