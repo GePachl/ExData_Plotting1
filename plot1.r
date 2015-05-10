9903c4c547f1ec63bab889d125b73e3d6943f019
Gerhard Pachl <gerhard.pachl@bluewin.ch>
2015-05-10 13:07:24 +0200

    Update Files Plot2

--- plot1.r
+++ plot1.r
@@ -1,24 +1,24 @@
 # Exploratory Data Analysis: Course Project 1
-# Task 1: Creating PNG Plot1 about "Global_active_power: household global minute-averaged active power (in kilowatt)" 
+# Task 1: Creating PNG Plot1 about "Global_active_power: household global minute-averaged active power (in kilowatt)"
+# for days 2007-02-01 (Thu), 2007-02-02 (Fri)" 
 
 # set my working directory
 setwd("C:/Users/Gerhard/ExData_Plotting1")
 
-#read project file and filter the data on relevant dates.
+# import course project data from file "Individual household electric power consumption Data Set"  
 hh_power_cons_data <- read.table("./household_power_consumption.txt",header = TRUE, sep = ";",na.strings="?",stringsAsFactors=FALSE, dec=".")
 
-# Change Date-Format (from dd/mm/yyyy to default format yyyy-mm-dd)
-hh_power_cons_data$Date <- as.Date(hh_power_cons_data$Date, format="%d/%m/%Y")
+# First reduce the dataset entries: select the relevant data for the plot - dates 1/2/2007 and 2/2/2007
+hh_power_consumption <- subset(hh_power_cons_data, hh_power_cons_data$Date == "1/2/2007" | hh_power_cons_data$Date == "2/2/2007") 
 
-# Select the relevant data for the plot - dates 2007-02-01 and 2007-02-02
-hh_power_consumption <- subset(hh_power_cons_data, hh_power_cons_data$Date == "2007-02-01" | hh_power_cons_data$Date == "2007-02-02")
-
-# create data for plot1 
-Global_active_power <- as.numeric(hh_power_consumption$Global_active_power)
+# Change chr-Format of Variable "Date" to Date-Format yyyy-mm-dd)
+hh_power_consumption$Date <- as.Date(hh_power_consumption$Date, format="%d/%m/%Y")
 
 # open graphics device png and create histogram (also set width of 480 pixels and a height of 480 pixels)
 png("plot1.png", width=480, height=480)
-hist(Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+
+# use the variable "Global_active_power" from Subset for Histogram
+hist(hh_power_consumption$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
 
 # close the png device
 dev.off()
