cd29277a93e91b1550858bf07b2dd9504e2ed099
Gerhard Pachl <gerhard.pachl@bluewin.ch>
2015-05-10 16:25:41 +0200

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -11,7 +11,7 @@
       ## import course project data from file "Individual household electric power consumption Data Set"  
       hh_power_cons_data <- read.table("./household_power_consumption.txt",header = TRUE, sep = ";",na.strings="?",stringsAsFactors=FALSE, dec=".")
       
-      # First reduce the dataset entries: select the relevant data for the plot - dates 1/2/2007 and 2/2/2007
+      # reduce the dataset entries: select the relevant data for the plot - dates 1/2/2007 and 2/2/2007
       hh_power_consumption <- subset(hh_power_cons_data, hh_power_cons_data$Date == "1/2/2007" | hh_power_cons_data$Date == "2/2/2007") 
       
       #  # create new variable "Date_time" for the plot - bind date and time and set appropriate format for the plot 
@@ -20,19 +20,20 @@
       # convert Date-Format from variable "Date" also (from dd/mm/yyyy to default format yyyy-mm-dd)
       hh_power_consumption$Date <- as.Date(hh_power_consumption$Date, format="%d/%m/%Y")
       
-      # open graphics device png and create plot2 (also set width of 480 pixels and a height of 480 pixels)
+      # open graphics device png for plot3 (set width of 480 pixels and a height of 480 pixels)
       png("plot3.png", width=480, height=480)
       
-      # Create the plot for relation of variable "Global_active_power"  new Var Date_time,with plot-type "1" for lines, xlab-axis without label
+      # create a multiple plot for the three types of "energy sub-metering"  
       with(hh_power_consumption, {
             plot(Sub_metering_1~Date_time, type="l", ylab="Energy sub metering", xlab="")
             lines(Sub_metering_2~Date_time,col='Red')
             lines(Sub_metering_3~Date_time,col='Blue')
       })
+      # add some labels and set properties for linetype
       legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
              legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
       
-      # close the png graphics device
+      # close png graphics device
       dev.off()
 
 
